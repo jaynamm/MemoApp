748ab4e80e7f9ae3f0759b8c492906540d8ddf89
jaynamm <polky0421@gmail.com>
2020-07-04 00:24:34 +0900

    display dummy memolist on table view and change timeset from UTC to KST

--- MemoApp/MemoListTableViewController.swift
+++ MemoApp/MemoListTableViewController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class MemoTableViewController: UITableViewController {
+class MemoListTableViewController: UITableViewController {
 
+    let formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.dateStyle = .long
+        f.timeStyle = .short
+        f.locale = Locale(identifier: "Ko_kr")
+        return f
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,25 +30,27 @@ class MemoTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return Memo.dummyMemoList.count
     }
 
-    /*
+    // 가장 중요한 메소드
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
         // Configure the cell...
+        let target = Memo.dummyMemoList[indexPath.row]
+        cell.textLabel?.text = target.contents
+        cell.detailTextLabel?.text = formatter.string(from: target.insertDate)
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
