ad65331541e287113f23788833f4ea02f10a5354
jaynamm <polky0421@gmail.com>
2020-07-04 00:10:53 +0900

    Memo Model create

---
+++ MemoApp/Memo/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  MemoApp
+//
+//  Created by 남정현 on 2020/07/04.
+//  Copyright © 2020 jaynam. All rights reserved.
+//
+
+import Foundation
+
+class Memo {
+    var contents: String
+    var insertDate: Date
+    
+    init(contents: String) {
+        self.contents = contents
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(contents: "메모장을 만들자~"),
+        Memo(contents: "다시 만들어 보는 메모장 ~~~ ❤️")
+    ]
+}
