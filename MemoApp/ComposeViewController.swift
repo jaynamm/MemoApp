2917f98ae703e4570031d3749e6a905dc5e0df6a
jaynamm <polky0421@gmail.com>
2020-07-05 00:55:40 +0900

    cancel button action set on new memo form

--- MemoApp/ComposeViewController.swift
+++ MemoApp/ComposeViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ComposeViewController: UIViewController {
 
+    
+    @IBAction func close(_ sender: Any) {
+        // 닫기 후 별도로 값을 전달하려면 completion 부분을 활용
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
